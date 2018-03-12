855f5741839b69c0b1c68e710569854eedb0b315
Jack Zampolin <jack.zampolin@gmail.com>
2018-03-12 14:41:28 -0700

    Add root package json

--- hub/scripts/run.sh
+++ hub/scripts/run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-npm install --prefix hub
 cp ./hub/config.sample.json ./hub/config.json
 sed -i "s/\"accessKeyId\": \"\",/\"accessKeyId\": \"$BUCKETEER_AWS_ACCESS_KEY_ID\",/g" ./hub/config.json
 sed -i "s/\"secretAccessKey\": \"\",/\"secretAccessKey\": \"$BUCKETEER_AWS_SECRET_ACCESS_KEY\",/g" ./hub/config.json
