0b69029ca0db852215f954e1cd9bd71d741a28cd
Aaron Blankstein <aaron@blockstack.com>
2018-03-13 14:17:44 -0400

    unique challenge token in heroku deploy

--- hub/scripts/run.sh
+++ hub/scripts/run.sh
@@ -6,7 +6,7 @@ sed -i "s#\"secretAccessKey\": \"\"#\"secretAccessKey\": \"$BUCKETEER_AWS_SECRET
 sed -i "s/\"readURL\": \"\",/\"readURL\": \"$BUCKETEER_BUCKET_NAME.s3.amazonaws.com\",/g" ./hub/config.json
 sed -i "s/\"driver\": \"\",/\"driver\": \"aws\",/g" ./hub/config.json
 sed -i "s/\"bucket\": \"\",/\"bucket\": \"$BUCKETEER_BUCKET_NAME\",/g" ./hub/config.json
-sed -i "s/\"servername\": \"\",/\"servername\": \"$URL\",/g" ./hub/config.json
+sed -i "s/\"servername\": \"\",/\"servername\": \"$CHALLENGE_TOKEN\",/g" ./hub/config.json
 sed -i "s/\"port\": 3000,/\"port\": $PORT,/g" ./hub/config.json
 sed -i "s/\"proofsRequired\" : 3/\"proofsRequired\": 0/g" ./hub/config.json
 sed -i "s/\"warn\"/\"debug\"/g" ./hub/config.json
