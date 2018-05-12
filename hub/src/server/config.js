e573c05d24406fd06838a31c94e81005f69a356b
bo <robert@jbanetwork.com>
2018-05-12 16:34:44 -0400

    Use convict for config validation, defaults, and ENV variable support standardize winston import to `logger`

--- hub/src/server/config.js
+++ hub/src/server/config.js
@@ -1,26 +1,51 @@
-import winston from 'winston'
+import convict from 'convict'
 import fs from 'fs'
 import process from 'process'
+import logger from 'winston'
 
-const configDefaults = {
-  argsTransport: {
-    level: 'warn',
-    handleExceptions: true,
-    timestamp: true,
-    stringify: true,
-    colorize: true,
-    json: true
+convict.addFormat({
+  // This format will convert a comma separated string
+  // to an array so it can be set via ENV variable
+  name: 'arrayOrFalse',
+  validate: function(val) {
+    // An array is acceptable
+    if (typeof val === 'object' && Array.isArray(val)) {
+      return
+    }
+
+    // false is also acceptable, everything else should throw an error
+    if (val !== false) {
+      throw new TypeError('must evaluate to false, be a comma separated list, or an Array')
+    }
+  },
+  coerce: function(val) {
+    if (val) {
+      if (typeof val == 'string'){
+        return val.replace(/\s/g, '').split(',')
+      }
+    }
+    else {
+      return false
+    }
   }
-}
+})
 
 export function getConfig() {
+  const config = convict('./convict-config.json')
+
   const configPath = process.env.CONFIG_PATH || process.argv[2] || './config.json'
-  const config = Object.assign(
-    {}, configDefaults, JSON.parse(fs.readFileSync(configPath)))
 
-  winston.configure({ transports: [
-    new winston.transports.Console(config.argsTransport) ] })
+  if (fs.existsSync(configPath)) {
+    config.loadFile(configPath)
+  }
 
-  return config
+  config.validate({allowed: 'strict'})
+
+  const configProperties = config.getProperties()
+
+  logger.configure({ transports: [
+    new logger.transports.Console(configProperties.argsTransport) ] })
+
+  return configProperties
 }
 
