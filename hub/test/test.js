f0cc8d155afb2d468126ed037cec0ff0a291abdc
Jude Nelson <jude@blockstack.com>
2018-09-10 17:44:02 -0400

    test that handleRequest() honors auth scopes in an authentication token correctly.  Make sure they work with and without association tokens

--- hub/test/test.js
+++ hub/test/test.js
@@ -359,12 +359,6 @@ function testAuth() {
       return new auth.V1Authentication(auth.V1Authentication.fromAuthPart(auth.V1Authentication.makeAuthPart(keypair, ct, assocJWT)).token)
     }
 
-    // makeAssocAuthToken(testPairs[1], challengeText, associationTokenWithoutIssuer).isAuthenticationValid(testAddrs[1], challengeText)
-    // makeAssocAuthToken(testPairs[1], challengeText, associationTokenWithoutExp).isAuthenticationValid(testAddrs[1], challengeText)
-    // makeAssocAuthToken(testPairs[1], challengeText, expiredAssociationToken).isAuthenticationValid(testAddrs[1], challengeText)
-    // makeAssocAuthToken(testPairs[1], challengeText, wrongIssuerAssociationToken).isAuthenticationValid(testAddrs[1], challengeText)
-    // makeAssocAuthToken(testPairs[1], challengeText, wrongBearerAddressAssociationToken).isAuthenticationValid(testAddrs[1], challengeText)
-
     t.throws(() => makeAssocAuthToken(testPairs[1], challengeText, associationTokenWithoutIssuer).isAuthenticationValid(testAddrs[1], challengeText),
              errors.ValidationError, 'No `iss` in association token, should fail')
     t.throws(() => makeAssocAuthToken(testPairs[1], challengeText, associationTokenWithoutExp).isAuthenticationValid(testAddrs[1], challengeText),
@@ -376,6 +370,94 @@ function testAuth() {
     t.throws(() => makeAssocAuthToken(testPairs[1], challengeText, wrongBearerAddressAssociationToken).isAuthenticationValid(testAddrs[1], challengeText),
              errors.ValidationError, 'Wrong bearer address for association token, should fail')
 
+    t.end()
+  })
+
+  test('v1 storage validation with scoped authentication token', (t) => {
+    const challengeText = 'bananas are tasty'
+    const writeScopes = [
+      {
+        scope: 'putFile',
+        domain: '/foo/bar',
+      },
+      {
+        scope: 'putFile',
+        domain: '/baz'
+      }
+    ]
+
+    const writeScopesInvalid = [
+      {
+        scope: 'invalid',
+        domain: 'nope',
+      }
+    ]
+
+    const writeScopesTooLong = [
+      {
+        scope: 'putFile',
+        domain: '/0',
+      },
+      {
+        scope: 'putFile',
+        domain: '/1'
+      },
+      {
+        scope: 'putFile',
+        domain: '/2'
+      },
+      {
+        scope: 'putFile',
+        domain: '/3'
+      },
+      {
+        scope: 'putFile',
+        domain: '/4'
+      },
+      {
+        scope: 'putFile',
+        domain: '/5'
+      },
+      {
+        scope: 'putFile',
+        domain: '/6'
+      },
+      {
+        scope: 'putFile',
+        domain: '/7'
+      },
+      {
+        scope: 'putFile',
+        domain: '/8'
+      }
+    ]
+    
+    const authPart = auth.V1Authentication.makeAuthPart(testPairs[0], challengeText, undefined, undefined, writeScopes)
+    console.log(`V1 storage validation: ${authPart}`)
+
+    const authorization = `bearer ${authPart}`
+    const authenticator = auth.parseAuthHeader(authorization)
+    t.throws(() => authenticator.isAuthenticationValid(testAddrs[1], challengeText),
+             errors.ValidationError, 'Wrong address must throw')
+    t.throws(() => authenticator.isAuthenticationValid(testAddrs[0], 'potatos are tasty'),
+             errors.ValidationError, 'Wrong challenge text must throw')
+    t.ok(authenticator.isAuthenticationValid(testAddrs[0], challengeText),
+         'Good signature must pass')
+
+    // scopes must be present 
+    const authScopes = authenticator.getAuthenticationScopes()
+    t.equal(authScopes[0].scope, 'putFile', 'scope 0 is putfile')
+    t.equal(authScopes[0].domain, '/foo/bar', 'scope 0 is for /foo/bar')
+    t.equal(authScopes[1].scope, 'putFile', 'scope 1 is putfile')
+    t.equal(authScopes[1].domain, '/baz', 'scope 1 is for /baz')
+
+    // invalid scopes must not be allowed 
+    t.throws(() => auth.V1Authentication.makeAuthPart(testPairs[0], challengeText, undefined, undefined, writeScopesTooLong),
+      errors.ValidationError, 'Too many scopes must throw')
+    
+    t.throws(() => auth.V1Authentication.makeAuthPart(testPairs[0], challengeText, undefined, undefined, writeScopesInvalid),
+      errors.ValidationError, 'Invalid scopes must throw')
+    
     t.end()
   })
 }
@@ -608,7 +690,7 @@ function testGcDriver() {
 
 function testServer() {
   test('validation tests', (t) => {
-    t.plan(3)
+    t.plan(4)
     const server = new HubServer(new MockDriver(), new MockProofs(),
                                  { whitelist: [testAddrs[0]] })
     t.throws(() => server.validate(testAddrs[1], {}),
@@ -624,6 +706,13 @@ function testServer() {
       t.pass('White-listed address with good auth header should pass')
     } catch (err) {
       t.fail('White-listed address with good auth header should pass')
+    }
+
+    try {
+      server.validate(testAddrs[1], { authorization })
+      t.fail('Non white-listed address with good auth header should fail')
+    } catch (err) {
+      t.pass('Non white-listed address with good auth header should fail')
     }
   })
 
@@ -735,6 +824,182 @@ function testServer() {
         t.equal(mockDriver.lastWrite.path, 'foo.txt')
         t.equal(mockDriver.lastWrite.storageTopLevel, testAddrs[0])
         t.equal(mockDriver.lastWrite.contentType, 'application/octet-stream')
+      })
+  })
+
+  test('handle scoped writes', (t) => {
+
+    const writeScopes = [
+      {
+        scope: 'putFile',
+        domain: '/foo/bar',
+      },
+      {
+        scope: 'putFilePrefix',
+        domain: 'baz'
+      }
+    ]
+
+    const mockDriver = new MockDriver()
+    const server = new HubServer(mockDriver, new MockProofs(),
+                                 { whitelist: [testAddrs[0]] })
+    const challengeText = auth.getChallengeText()
+
+    const authPart = auth.V1Authentication.makeAuthPart(testPairs[0], challengeText, undefined, undefined, writeScopes)
+    
+    console.log(`V1 storage validation: ${authPart}`)
+
+    const authorization = `bearer ${authPart}`
+    const authenticator = auth.parseAuthHeader(authorization)
+    t.throws(() => authenticator.isAuthenticationValid(testAddrs[1], challengeText),
+             errors.ValidationError, 'Wrong address must throw')
+    t.throws(() => authenticator.isAuthenticationValid(testAddrs[0], 'potatos are tasty'),
+             errors.ValidationError, 'Wrong challenge text must throw')
+    t.ok(authenticator.isAuthenticationValid(testAddrs[0], challengeText),
+         'Good signature must pass')
+
+    // scopes must be present 
+    const authScopes = authenticator.getAuthenticationScopes()
+    t.equal(authScopes[0].scope, 'putFile', 'scope 0 is putfile')
+    t.equal(authScopes[0].domain, '/foo/bar', 'scope 0 is for /foo/bar')
+    t.equal(authScopes[1].scope, 'putFilePrefix', 'scope 1 is putFilePrefix')
+    t.equal(authScopes[1].domain, 'baz', 'scope 1 is for baz')
+
+    // write to /foo/bar or /baz will succeed
+    const s = new Readable()
+    s._read = function noop() {}
+    s.push('hello world')
+    s.push(null)
+    const s2 = new Readable()
+    s2._read = function noop() {}
+    s2.push('hello world')
+    s2.push(null)
+    const s3 = new Readable()
+    s3._read = function noop() {}
+    s3.push('hello world')
+    s3.push(null)
+    const s4 = new Readable()
+    s4._read = function noop() {}
+    s4.push('hello world')
+    s4.push(null)
+
+    server.handleRequest(testAddrs[0], '/foo/bar',
+                         { 'content-type' : 'text/text',
+                           'content-length': 4,
+                           authorization }, s)
+      .then(path => {
+        // NOTE: the double-/ is *expected*
+        t.equal(path, `http://test.com/${testAddrs[0]}//foo/bar`)
+        t.equal(mockDriver.lastWrite.path, '/foo/bar')
+        t.equal(mockDriver.lastWrite.storageTopLevel, testAddrs[0])
+        t.equal(mockDriver.lastWrite.contentType, 'text/text')
+      })
+      .then(() => server.handleRequest(testAddrs[0], 'baz/foo.txt',
+                         { 'content-length': 4,
+                           authorization }, s2))
+      .then(path => {
+        t.equal(path, `http://test.com/${testAddrs[0]}/baz/foo.txt`)
+        t.equal(mockDriver.lastWrite.path, 'baz/foo.txt')
+        t.equal(mockDriver.lastWrite.storageTopLevel, testAddrs[0])
+        t.equal(mockDriver.lastWrite.contentType, 'application/octet-stream')
+      })
+      .then(() => server.handleRequest(testAddrs[0], '/nope/foo.txt',
+                         { 'content-length': 4,
+                           authorization }, s3))
+      .catch((e) => {
+        t.throws(() => { throw e }, errors.ValidationError, 'invalid path prefix should fail')
+      })
+      .then(() => server.handleRequest(testAddrs[0], '/foo/bar/nope.txt',
+                                      { 'content-length': 4,
+                                      authorization }, s4))
+      .catch((e) => {
+        t.throws(() => { throw e }, errors.ValidationError, 'putFile does not allow prefixes')
+        t.end()
+      })
+  })
+
+  test('handle scoped writes with association tokens', (t) => {
+
+    const writeScopes = [
+      {
+        scope: 'putFile',
+        domain: '/foo/bar',
+      },
+      {
+        scope: 'putFilePrefix',
+        domain: 'baz'
+      }
+    ]
+
+    const mockDriver = new MockDriver()
+    const server = new HubServer(mockDriver, new MockProofs(),
+                                 { whitelist: [testAddrs[1]] })
+    const challengeText = auth.getChallengeText()
+
+    const associationToken = auth.V1Authentication.makeAssociationToken(testPairs[1], testPairs[0].getPublicKeyBuffer().toString('hex'))
+    const authPart = auth.V1Authentication.makeAuthPart(testPairs[0], challengeText, associationToken, undefined, writeScopes)
+
+    console.log(`V1 storage validation: ${authPart}`)
+
+    const authorization = `bearer ${authPart}`
+    const authenticator = auth.parseAuthHeader(authorization)
+    t.throws(() => authenticator.isAuthenticationValid(testAddrs[1], challengeText),
+             errors.ValidationError, 'Wrong address must throw')
+    t.throws(() => authenticator.isAuthenticationValid(testAddrs[0], 'potatos are tasty'),
+             errors.ValidationError, 'Wrong challenge text must throw')
+    t.ok(authenticator.isAuthenticationValid(testAddrs[0], challengeText),
+         'Good signature must pass')
+
+    // write to /foo/bar or baz will succeed
+    const s = new Readable()
+    s._read = function noop() {}
+    s.push('hello world')
+    s.push(null)
+    const s2 = new Readable()
+    s2._read = function noop() {}
+    s2.push('hello world')
+    s2.push(null)
+    const s3 = new Readable()
+    s3._read = function noop() {}
+    s3.push('hello world')
+    s3.push(null)
+    const s4 = new Readable()
+    s4._read = function noop() {}
+    s4.push('hello world')
+    s4.push(null)
+
+    server.handleRequest(testAddrs[0], '/foo/bar',
+                         { 'content-type' : 'text/text',
+                           'content-length': 4,
+                           authorization }, s)
+      .then(path => {
+        // NOTE: the double-/ is *expected*
+        t.equal(path, `http://test.com/${testAddrs[0]}//foo/bar`)
+        t.equal(mockDriver.lastWrite.path, '/foo/bar')
+        t.equal(mockDriver.lastWrite.storageTopLevel, testAddrs[0])
+        t.equal(mockDriver.lastWrite.contentType, 'text/text')
+      })
+      .then(() => server.handleRequest(testAddrs[0], 'baz/foo.txt',
+                         { 'content-length': 4,
+                           authorization }, s2))
+      .then(path => {
+        t.equal(path, `http://test.com/${testAddrs[0]}/baz/foo.txt`)
+        t.equal(mockDriver.lastWrite.path, 'baz/foo.txt')
+        t.equal(mockDriver.lastWrite.storageTopLevel, testAddrs[0])
+        t.equal(mockDriver.lastWrite.contentType, 'application/octet-stream')
+      })
+      .then(() => server.handleRequest(testAddrs[0], '/nope/foo.txt',
+                         { 'content-length': 4,
+                           authorization }, s3))
+      .catch((e) => {
+        t.throws(() => { throw e }, errors.ValidationError, 'invalid prefix should fail')
+      })
+      .then(() => server.handleRequest(testAddrs[0], '/foo/bar/nope.txt',
+                        { 'content-length': 4,
+                          authorization }, s4 ))
+      .catch((e) => {
+        t.throws(() => { throw e }, errors.ValidationError, 'putFile does not permit prefixes')
+        t.end()
       })
   })
 }
