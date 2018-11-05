18e78c360dad414ab62afb435a8448b9911c42f8
Aaron Blankstein <aaron@blockstack.com>
2018-11-05 09:28:31 -0600

    start breaking up the unit test megafile

---
+++ hub/test/src/index.js
@@ -0,0 +1,10 @@
+import { testAuth } from './testAuth.js'
+import { testServer } from './testServer.js'
+import { testDrivers } from './testDrivers.js'
+
+
+
+testServer()
+testAuth()
+testDrivers()
+
