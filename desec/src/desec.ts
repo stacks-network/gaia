822f05adc306cb67b47de4a095498cc6e56420b1
Matthew Little <zone117x@gmail.com>
2019-08-09 14:58:00 -0400

    Implement domain record APIs

--- desec/src/desec.ts
+++ desec/src/desec.ts
@@ -2,6 +2,7 @@ import fetch from 'cross-fetch';
 
 const API_ENDPOINT = 'https://desec.io/api/v1';
 const USER_AGENT = 'blockstack-gaia-desec';
+const DEFAULT_ROOT_DOMAIN = 'dedyn.io';
 
 type HttpMethod = 'POST' | 'GET' | 'DELETE' | 'PUT' | 'PATCH';
 type ValidContentType = 'application/json';
@@ -53,13 +54,15 @@ export interface AccountInfoResult {
   locked: boolean;
 }
 
-export async function getAccountInfo(authToken: string): Promise<AccountInfoResult> {
+export async function getAccountInfo(opts: {
+  authToken: string;
+}): Promise<AccountInfoResult> {
   const url = `${API_ENDPOINT}/auth/me/`;
-  const opts = getDefaultFetchOpts({
+  const fetchOpts = getDefaultFetchOpts({
     method: 'GET',
-    authToken: authToken
+    authToken: opts.authToken
   });
-  const response = await fetch(url, opts);
+  const response = await fetch(url, fetchOpts);
   const apiError = await checkBadResponse(response);
   if (apiError) {
     throw apiError;
@@ -72,18 +75,21 @@ export interface RegisterResult {
   email: string;
 }
 
-export async function register(email: string, password: string): Promise<RegisterResult> {
+export async function register(opts: {
+  email: string;
+  password: string;
+}): Promise<RegisterResult> {
   const url = `${API_ENDPOINT}/auth/users/`;
   const data = {
-  	'email': email,
-    'password': password
+  	'email': opts.email,
+    'password': opts.password
   };
-  const opts = getDefaultFetchOpts({
+  const fetchOpts = getDefaultFetchOpts({
     method: 'POST',
     contentType: 'application/json',
     body: JSON.stringify(data)
   });
-  const response = await fetch(url, opts);
+  const response = await fetch(url, fetchOpts);
   const apiError = await checkBadResponse(response);
   if (apiError) {
     if (apiError.responseJson
@@ -102,18 +108,21 @@ export interface LoginResult {
   auth_token: string;
 }
 
-export async function login(email: string, password: string): Promise<LoginResult> {
+export async function login(opts: {
+  email: string;
+  password: string;
+}): Promise<LoginResult> {
   const url = `${API_ENDPOINT}/auth/token/login/`;
   const data = {
-  	'email': email,
-    'password': password
+  	'email': opts.email,
+    'password': opts.password
   };
-  const opts = getDefaultFetchOpts({
+  const fetchOpts = getDefaultFetchOpts({
     method: 'POST',
     contentType: 'application/json',
     body: JSON.stringify(data)
   });
-  const response = await fetch(url, opts);
+  const response = await fetch(url, fetchOpts);
   const apiError = await checkBadResponse(response);
   if (apiError) {
     throw apiError;
@@ -123,20 +132,20 @@ export async function login(email: string, password: string): Promise<LoginResul
 }
 
 /** Invalidate an auth token */
-export async function logout(authToken: string): Promise<void> {
+export async function logout(opts: {
+  authToken: string;
+}): Promise<void> {
   const url = `${API_ENDPOINT}/auth/token/logout/`;
-  const opts = getDefaultFetchOpts({
+  const fetchOpts = getDefaultFetchOpts({
     method: 'POST',
-    authToken: authToken
+    authToken: opts.authToken
   });
-  const response = await fetch(url, opts);
+  const response = await fetch(url, fetchOpts);
   const apiError = await checkBadResponse(response);
   if (apiError) {
     throw apiError;
   }
 }
-
-const DEFAULT_ROOT_DOMAIN = 'dedyn.io';
 
 export interface CreateDomainResult {
   /**
@@ -170,24 +179,23 @@ export interface CreateDomainResult {
   name: string;
 }
 
-export async function createDomain(
-  authToken: string, name: string, useDefaultRoot = true
-): Promise<CreateDomainResult> {
-  let fullDomainName = name;
-  if (useDefaultRoot) {
-    fullDomainName = `${name}.${DEFAULT_ROOT_DOMAIN}`;
-  }
+export async function createDomain(opts: {
+  authToken: string;
+  name: string;
+  useDefaultRoot?: boolean;
+}): Promise<CreateDomainResult> {
+  const fullDomainName = getFullDomainName(opts.name, opts.useDefaultRoot);
   const url = `${API_ENDPOINT}/domains/`;
   const data = {
     'name': fullDomainName
   };
-  const opts = getDefaultFetchOpts({
+  const fetchOpts = getDefaultFetchOpts({
     method: 'POST',
-    authToken: authToken,
+    authToken: opts.authToken,
     contentType: 'application/json',
     body: JSON.stringify(data)
   });
-  const response = await fetch(url, opts);
+  const response = await fetch(url, fetchOpts);
   const apiError = await checkBadResponse(response);
   if (apiError) {
     if (apiError.responseJson
@@ -250,18 +258,224 @@ export interface ListDomainsResult extends Array<DomainInfoResult> {
 
 }
 
-export async function listDomains(authToken: string): Promise<ListDomainsResult> {
+export async function listDomains(opts: {
+  authToken: string;
+}): Promise<ListDomainsResult> {
   const url = `${API_ENDPOINT}/domains/`;
-  const opts = getDefaultFetchOpts({
+  const fetchOpts = getDefaultFetchOpts({
     method: 'GET',
-    authToken: authToken
+    authToken: opts.authToken
   });
-  const response = await fetch(url, opts);
+  const response = await fetch(url, fetchOpts);
   const apiError = await checkBadResponse(response);
   if (apiError) {
     throw apiError;
   }
   const result: ListDomainsResult = await response.json();
+  return result;
+}
+
+export async function getDomainInfo(opts: {
+  authToken: string;
+  name: string;
+  useDefaultRoot?: boolean;
+}): Promise<DomainInfoResult> {
+  const fullDomainName = getFullDomainName(opts.name, opts.useDefaultRoot);
+  const url = `${API_ENDPOINT}/domains/${fullDomainName}/`;
+  const fetchOpts = getDefaultFetchOpts({
+    method: 'GET',
+    authToken: opts.authToken
+  });
+  const response = await fetch(url, fetchOpts);
+  const apiError = await checkBadResponse(response);
+  if (apiError) {
+    throw apiError;
+  }
+  const result: DomainInfoResult = await response.json();
+  return result;
+}
+
+export interface DomainResourceRecord {
+  /**
+   * Name of the zone to which the RRset belongs. 
+   * Note that the zone name does not follow immediately from the RRset name. 
+   * For example, the `com` zone contains an RRset of type `NS` for the name 
+   * `example.com.`, in order to set up the delegation to `example.com`’s DNS 
+   * operator. The DNS operator’s nameserver again has a similar `NS` RRset 
+   * which, this time however, belongs to the `example.com` zone.
+   */
+  domain: string;
+  /**
+   * The full DNS name of the RRset. If `subname` is empty, this is equal to 
+   * `:name.`, otherwise it is equal to `:subname.:name.`.
+   */
+  name: string;
+  /**
+   * Array of record content strings. Please note that when a record value contains 
+   * a domain name, it is in almost all cases required to add a final dot after 
+   * the domain name. This applies, for example, to the CNAME, MX, and SRV record 
+   * types. A typical MX value would thus be be 10 mx.example.com. (note the 
+   * trailing dot). Please also consider the caveat on the priority field. The 
+   * maximum number of array elements is 4091, and the maximum length of the array 
+   * is 64,000 (after JSON encoding).
+   */
+  records: string[];
+  /**
+   * Subdomain string which, together with domain, defines the RRset name. Typical 
+   * examples are www or _443._tcp. In general, a subname consists of lowercase 
+   * alphanumeric characters as well as hyphens -, underscores _, and dots .. 
+   * Wildcard name components are denoted by *; this is allowed only once at the 
+   * beginning of the name (see RFC 4592 for details). The maximum length is 178. 
+   * Further restrictions may apply on a per-user basis.
+   */
+  subname: string;
+  /**
+   * TTL (time-to-live) value, which dictates for how long resolvers may cache this 
+   * RRset, measured in seconds. The smallest acceptable value is given by the 
+   * domain’s minimum TTL setting. The maximum value is 604800 (one week).
+   */
+  ttl: number;
+  /**
+   * RRset type (uppercase). We support all RRset types supported by PowerDNS, with 
+   * the exception of DNSSEC-related types (the backend automagically takes care of 
+   * setting those records properly). You also cannot access the SOA, see SOA caveat.
+   */
+  type: string;
+}
+
+interface CreateRecordSetOptions extends 
+  Pick<DomainResourceRecord, 'type' | 'records' | 'ttl'>, 
+  Partial<Pick<DomainResourceRecord, 'subname'>> { }
+
+interface UpdateRecordSetOptions extends 
+  Pick<DomainResourceRecord, 'type'>, 
+  Partial<Pick<DomainResourceRecord, 'records' | 'ttl' | 'subname'>> { }
+
+interface DeleteRecordSetOptions extends 
+  Pick<DomainResourceRecord, 'type'>,
+  Partial<Pick<DomainResourceRecord, 'subname'>> { } { }
+
+function getFullDomainName(name: string, useDefaultRoot = true): string {
+  if (useDefaultRoot && !name.endsWith(`.${DEFAULT_ROOT_DOMAIN}`)) {
+    return `${name}.${DEFAULT_ROOT_DOMAIN}`;
+  }
+  return name;
+}
+
+export async function getDomainRecordSets(opts: {
+  authToken: string;
+  name: string;
+  useDefaultRoot?: boolean;
+  filterType?: string;
+}): Promise<DomainResourceRecord[]> {
+  const fullDomainName = getFullDomainName(opts.name, opts.useDefaultRoot);
+  let url = `${API_ENDPOINT}/domains/${fullDomainName}/rrsets/`;
+  if (opts.filterType !== undefined) {
+    url += `?type=${opts.filterType}`;
+  }
+  const fetchOpts = getDefaultFetchOpts({
+    method: 'GET',
+    authToken: opts.authToken
+  });
+  const response = await fetch(url, fetchOpts);
+  const apiError = await checkBadResponse(response);
+  if (apiError) {
+    throw apiError;
+  }
+  const result: DomainResourceRecord[] = await response.json();
+  return result;
+}
+
+export async function createDomainRecordSet(opts: {
+  recordSet: CreateRecordSetOptions | CreateRecordSetOptions[];
+  authToken: string;
+  name: string;
+  useDefaultRoot?: boolean;
+}): Promise<DomainResourceRecord | DomainResourceRecord[]> {
+  const fullDomainName = getFullDomainName(opts.name, opts.useDefaultRoot);
+  const url = `${API_ENDPOINT}/domains/${fullDomainName}/rrsets/`;
+  const fetchOpts = getDefaultFetchOpts({
+    method: 'POST',
+    authToken: opts.authToken,
+    contentType: 'application/json',
+    body: JSON.stringify(opts.recordSet)
+  });
+  const response = await fetch(url, fetchOpts);
+  const apiError = await checkBadResponse(response);
+  if (apiError) {
+    throw apiError;
+  }
+  const result = await response.json();
+  return result;
+}
+
+export async function updateDomainRecordSet(opts: {
+  recordSet: UpdateRecordSetOptions | UpdateRecordSetOptions[];
+  authToken: string;
+  name: string;
+  useDefaultRoot?: boolean;
+}): Promise<DomainResourceRecord[]> {
+  const fullDomainName = getFullDomainName(opts.name, opts.useDefaultRoot);
+  const url = `${API_ENDPOINT}/domains/${fullDomainName}/rrsets/`;
+  let recordSet: UpdateRecordSetOptions[];
+  if (Array.isArray(opts.recordSet)) {
+    recordSet = opts.recordSet;
+  } else {
+    recordSet = [opts.recordSet];
+  }
+  recordSet.forEach(record => {
+    if (record.subname === undefined) {
+      record.subname = '';
+    }
+  });
+  const fetchOpts = getDefaultFetchOpts({
+    method: 'PATCH',
+    authToken: opts.authToken,
+    contentType: 'application/json',
+    body: JSON.stringify(recordSet)
+  });
+  const response = await fetch(url, fetchOpts);
+  const apiError = await checkBadResponse(response);
+  if (apiError) {
+    throw apiError;
+  }
+  const result = await response.json();
+  return result;
+}
+
+export async function deleteDomainRecordSet(opts: {
+  recordSet: DeleteRecordSetOptions | DeleteRecordSetOptions[];
+  authToken: string;
+  name: string;
+  useDefaultRoot?: boolean;
+}): Promise<void> {
+  let recordSet: (DeleteRecordSetOptions & { records: string[]; })[];
+  if (Array.isArray(opts.recordSet)) {
+    recordSet = opts.recordSet.map(item => {
+      return Object.assign(item, { records: [] });
+    });
+  } else {
+    recordSet = [Object.assign(opts.recordSet, { records: [] })];
+  }
+  recordSet.forEach(record => {
+    if (record.subname === undefined) {
+      record.subname = '';
+    }
+  });
+  const fullDomainName = getFullDomainName(opts.name, opts.useDefaultRoot);
+  const url = `${API_ENDPOINT}/domains/${fullDomainName}/rrsets/`;
+  const fetchOpts = getDefaultFetchOpts({
+    method: 'PATCH',
+    authToken: opts.authToken,
+    contentType: 'application/json',
+    body: JSON.stringify(recordSet)
+  });
+  const response = await fetch(url, fetchOpts);
+  const apiError = await checkBadResponse(response);
+  if (apiError) {
+    throw apiError;
+  }
+  const result = await response.json();
   return result;
 }
 
--- desec/test/account.ts
+++ desec/test/account.ts
@@ -8,18 +8,25 @@ chai.use(chaiAsPromised);
 const TEST_ACCOUNT_EMAIL = 'desec_testing@tmailservices.com';
 const TEST_ACCOUNT_PASSWORD = 'winkle-guano-sphinx-equator';
 const TEST_ACCOUNT_TOKEN = 'kBg-lv0KIQctimYG2zjT.uEaqBkZ';
+const TEST_DOMAIN_NAME = 'desec_testing_winkle';
 
 describe('desec.io API tests', () => {
 
   let newToken: string | undefined;
 
   it('registration - email already taken', async () => {
-    const registerPromise = desec.register(TEST_ACCOUNT_EMAIL, TEST_ACCOUNT_PASSWORD);
+    const registerPromise = desec.register({
+      email: TEST_ACCOUNT_EMAIL, 
+      password: TEST_ACCOUNT_PASSWORD
+    });
     await assert.isRejected(registerPromise, desec.EmailAlreadyRegisteredError);
   });
 
   it('login', async () => {
-    const result = await desec.login(TEST_ACCOUNT_EMAIL, TEST_ACCOUNT_PASSWORD);
+    const result = await desec.login({
+      email: TEST_ACCOUNT_EMAIL, 
+      password: TEST_ACCOUNT_PASSWORD
+    });
     newToken = result.auth_token;
     assert.isString(newToken);
     assert.isAbove(newToken.length, 1);
@@ -27,30 +34,107 @@ describe('desec.io API tests', () => {
 
   it('[logout] destroy new token', async () => {
     if (newToken) {
-      const logoutResult = await desec.logout(newToken);
+      await desec.logout({
+        authToken: newToken
+      });
     } else {
       throw new Error('No new auth token to destroy');
     }
   });
 
   it('get account info', async () => {
-    const info = await desec.getAccountInfo(TEST_ACCOUNT_TOKEN);
+    const info = await desec.getAccountInfo({
+      authToken: TEST_ACCOUNT_TOKEN
+    });
     assert.equal(info.email, TEST_ACCOUNT_EMAIL);
     assert.equal(info.limit_domains, 5);
     assert.equal(info.locked, false);
   });
 
   it('create domain', async () => {
-    const createDomainPromise = desec.createDomain(TEST_ACCOUNT_TOKEN, 'desec_testing_winkle');
+    const createDomainPromise = desec.createDomain({
+      authToken: TEST_ACCOUNT_TOKEN, 
+      name: TEST_DOMAIN_NAME
+    });
     await assert.isRejected(createDomainPromise, desec.DomainNameAlreadyExistsError);
   });
 
   it('list domains', async () => {
-    const domainList = await desec.listDomains(TEST_ACCOUNT_TOKEN);
-    const domain = domainList.find(d => d.name === 'desec_testing_winkle.dedyn.io');
+    const domainList = await desec.listDomains({
+      authToken: TEST_ACCOUNT_TOKEN
+    });
+    const domain = domainList.find(d => d.name === `${TEST_DOMAIN_NAME}.dedyn.io`);
     if (!domain) {
       throw new Error('Should have found domain');
     }
-  })
+  });
+
+  it('get domain info', async () => {
+    const domainInfo = await desec.getDomainInfo({
+      authToken: TEST_ACCOUNT_TOKEN,
+      name: TEST_DOMAIN_NAME
+    });
+    assert.strictEqual(domainInfo.created, '2019-08-09T02:17:30Z');
+    assert.strictEqual(domainInfo.name, `${TEST_DOMAIN_NAME}.dedyn.io`);
+    // assert.strictEqual(domainInfo.published, '2019-08-09T02:17:30.375201Z');
+  });
+
+  it('get domain records', async () => {
+    const records = await desec.getDomainRecordSets({
+      authToken: TEST_ACCOUNT_TOKEN,
+      name: TEST_DOMAIN_NAME
+    });
+    assert.ok(records);
+  });
+
+  it('create record set', async () => {
+    await desec.createDomainRecordSet({
+      authToken: TEST_ACCOUNT_TOKEN,
+      name: TEST_DOMAIN_NAME,
+      recordSet: {
+        ttl: 60,
+        type: 'A',
+        records: ['1.2.3.4']
+      }
+    });
+    // await assert.isRejected(createPromise, /same subdomain and type exists for this domain/);
+  });
+
+  it('update record set', async () => {
+    await desec.updateDomainRecordSet({
+      authToken: TEST_ACCOUNT_TOKEN,
+      name: TEST_DOMAIN_NAME,
+      recordSet: {
+        ttl: 60,
+        type: 'A',
+        records: ['2.2.2.2']
+      }
+    });
+    const recordSets = await desec.getDomainRecordSets({
+      authToken: TEST_ACCOUNT_TOKEN,
+      name: TEST_DOMAIN_NAME,
+      filterType: 'A'
+    });
+    assert.isArray(recordSets);
+    assert.lengthOf(recordSets, 1);
+    assert.sameMembers(recordSets[0].records, ['2.2.2.2']);
+  });
+
+  it('delete record set', async () => {
+    await desec.deleteDomainRecordSet({
+      authToken: TEST_ACCOUNT_TOKEN,
+      name: TEST_DOMAIN_NAME,
+      recordSet: {
+        type: 'A'
+      }
+    });
+    const records = await desec.getDomainRecordSets({
+      authToken: TEST_ACCOUNT_TOKEN,
+      name: TEST_DOMAIN_NAME,
+      filterType: 'A'
+    });
+    assert.isArray(records);
+    assert.lengthOf(records, 0);
+  });
 
 });
