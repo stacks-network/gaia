88dd68c045c622a82708f8f5e5b6f4cdc16b252b
wileyj <2847772+wileyj@users.noreply.github.com>
2021-05-03 16:56:45 -0400

    updates to gaia setup script

--- deploy/packer/scripts/gaia.sh
+++ deploy/packer/scripts/gaia.sh
@@ -4,14 +4,13 @@ sudo git clone -b great-gaia-guidance  --depth 1 https://github.com/blockstack/g
 
 echo === Copying files ===
 sudo mkdir -p /storage /gaia
-sudo cp -R /root/gaia/deploy/packer/system-files/etc/motd.d/default.conf /etc/motd.d/default.conf
 sudo cp -R /root/gaia/deploy/packer/system-files/etc/modules-load.d/nf.conf /etc/modules-load.d/nf.conf 
-# cp -R /gaia/deploy/packer/system-files/etc/sysctl.d/startup.conf /etc/sysctl.d/startup.conf
-sudo cp -R /root/gaia/deploy/unit-files/* /etc/systemd/system/
+sudo cp -R /root/gaia/deploy/packer/system-files/etc/sysctl.d/startup.conf /etc/sysctl.d/startup.conf
+for FILE in $(sudo ls /root/gaia/deploy/unit-files); do
+  sudo cp -a /root/gaia/deploy/unit-files/${FILE} /etc/systemd/system/${FILE}
+done
 sudo cp -R /root/gaia/deploy/scripts /gaia/
-sudo cp -R /root/gaia/deploy/admin-config /gaia/admin-config
-sudo cp -R /root/gaia/deploy/reader-config /gaia/reader-config
-sudo cp -R /root/gaia/deploy/hub-config /gaia/hub-config
+sudo cp -R /root/gaia/deploy/configs /gaia/
 sudo cp -R /root/gaia/deploy/.env /gaia/gaia.env
 sudo cp -R /root/gaia/deploy/docker-compose.yaml /gaia/docker-compose.yaml
 
