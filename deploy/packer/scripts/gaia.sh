f0daff062dd1c795d7ff731a32c328d2b17cc4d9
CharlieC3 <2747302+CharlieC3@users.noreply.github.com>
2021-05-05 13:33:12 -0400

    chore: dont checkout in root

--- deploy/packer/scripts/gaia.sh
+++ deploy/packer/scripts/gaia.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 echo === Cloning gaia repo ===
-sudo git clone -b great-gaia-guidance  --depth 1 https://github.com/blockstack/gaia /root/gaia
+sudo git clone -b great-gaia-guidance --depth 1 https://github.com/blockstack/gaia /gaia
 
 echo === Configuring Boot Scripts ===
 sudo mkdir -p /gaia
-sudo cat <<EOF> /etc/rc.local
+sudo cat <<'EOF'> /etc/rc.local
 #!/bin/sh -e
 #
 # rc.local
@@ -14,10 +14,11 @@ sudo cat <<EOF> /etc/rc.local
 # value on error.
 source /usr/local/bin/aws_tags || exit 1
 
-cp /root/gaia/deploy/docker/sample-aws.env /gaia/docker/aws.env
-sed -i "s/DOMAIN_NAME=\".*\"/DOMAIN_NAME=\"$Domain\"/g" /root/gaia/deploy/docker/aws.env
-sed -i "s/CERTBOT_EMAIL=\".*\"/CERTBOT_EMAIL=\"$Email\"/g" /root/gaia/deploy/docker/aws.env
-sed -i "s/GAIA_BUCKET_NAME=\".*\"/GAIA_BUCKET_NAME=\"$BucketName\"/g" /root/gaia/deploy/docker/aws.env
+echo === Configuring Gaia ===
+cp /gaia/deploy/docker/sample-aws.env /gaia/deploy/docker/aws.env
+sed -i "s/DOMAIN_NAME=\".*\"/DOMAIN_NAME=\"$Domain\"/g" /gaia/deploy/docker/aws.env
+sed -i "s/CERTBOT_EMAIL=\".*\"/CERTBOT_EMAIL=\"$Email\"/g" /gaia/deploy/docker/aws.env
+sed -i "s/GAIA_BUCKET_NAME=\".*\"/GAIA_BUCKET_NAME=\"$BucketName\"/g" /gaia/deploy/docker/aws.env
 exit 0
 EOF
 
@@ -26,14 +27,10 @@ sudo mv /tmp/aws_tags /usr/local/bin/aws_tags
 sudo chmod 755 /usr/local/bin/aws_tags
 
 echo === Copying files ===
-sudo cp -R /root/gaia/deploy/packer/system-files/etc/modules-load.d/nf.conf /etc/modules-load.d/nf.conf 
-sudo cp -R /root/gaia/deploy/packer/system-files/etc/sysctl.d/startup.conf /etc/sysctl.d/startup.conf
-for FILE in $(sudo ls /root/gaia/deploy/unit-files); do
-  sudo cp -a /root/gaia/deploy/unit-files/${FILE} /etc/systemd/system/${FILE}
+sudo cp -R /gaia/deploy/packer/system-files/etc/modules-load.d/nf.conf /etc/modules-load.d/nf.conf 
+sudo cp -R /gaia/deploy/packer/system-files/etc/sysctl.d/startup.conf /etc/sysctl.d/startup.conf
+for FILE in $(sudo ls /gaia/deploy/unit-files); do
+  sudo cp -a /gaia/deploy/unit-files/${FILE} /etc/systemd/system/${FILE}
 done
 
-sudo cp -R /root/gaia/deploy/configs /gaia/
-sudo cp -R /root/gaia/deploy/docker /gaia/
-
 sudo systemctl enable gaia
-sudo rm -rf /root/gaia
