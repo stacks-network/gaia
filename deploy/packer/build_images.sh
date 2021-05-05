c31fa07f50cbb238151993425a96de76d85e2299
wileyj <2847772+wileyj@users.noreply.github.com>
2021-05-04 23:10:51 -0400

    update perms - cleanup

--- deploy/packer/build_images.sh
+++ deploy/packer/build_images.sh
@@ -8,9 +8,16 @@ echo "Setting Version to ${VERSION}"
 # echo "    json: gaia.json"
 # packer build --var-file=vars.json --var "version=${VERSION}" gaia-ephemeral.json
 
+# echo ""
+# echo "Building gaia-hub-ebs"
+# echo "    user_data_file: gaia-ebs.ign"
+# echo "    version: ${VERSION}"
+# echo "    json: gaia-ebs.json"
+# packer build --var-file=vars.json --var "version=${VERSION}" gaia-ebs.json
+
 echo ""
-echo "Building gaia-hub-ebs"
-echo "    user_data_file: gaia-ebs.ign"
+echo "Building gaia-hub"
 echo "    version: ${VERSION}"
-echo "    json: gaia-ebs.json"
-packer build --var-file=vars.json --var "version=${VERSION}" gaia-ebs.json
+echo "    json: gaia.json"
+packer build --var-file=vars.json --var "version=${VERSION}" gaia.json
+
--- deploy/packer/scripts/gaia.sh
+++ deploy/packer/scripts/gaia.sh
@@ -3,14 +3,12 @@ echo === Cloning gaia repo ===
 sudo git clone -b great-gaia-guidance  --depth 1 https://github.com/blockstack/gaia /root/gaia
 
 echo === Copying files ===
-sudo mkdir -p /storage /gaia
+sudo mkdir -p /gaia
 sudo cp -R /root/gaia/deploy/packer/system-files/etc/modules-load.d/nf.conf /etc/modules-load.d/nf.conf 
 sudo cp -R /root/gaia/deploy/packer/system-files/etc/sysctl.d/startup.conf /etc/sysctl.d/startup.conf
-for FILE in $(sudo ls /root/gaia/deploy/unit-files); do
-  sudo cp -a /root/gaia/deploy/unit-files/${FILE} /etc/systemd/system/${FILE}
-done
-sudo cp -R /root/gaia/deploy/scripts /gaia/
+# for FILE in $(sudo ls /root/gaia/deploy/unit-files); do
+#   sudo cp -a /root/gaia/deploy/unit-files/${FILE} /etc/systemd/system/${FILE}
+# done
 sudo cp -R /root/gaia/deploy/configs /gaia/
-sudo cp -R /root/gaia/deploy/.env /gaia/gaia.env
-sudo cp -R /root/gaia/deploy/docker-compose.yaml /gaia/docker-compose.yaml
-
+sudo cp -R /root/gaia/deploy/docker /gaia/
+#systemctl disable gaia
