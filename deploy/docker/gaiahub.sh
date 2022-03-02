8289f5e66f765a6a5fd1cc568c236a8a78b8c84a
Alejandro Criado-Pérez <alejandro@criadoperez.com>
2022-03-02 18:49:32 +0100

    code-review completed

--- deploy/docker/gaiahub.sh
+++ deploy/docker/gaiahub.sh
@@ -24,18 +24,18 @@ check_files_exist() {
 	# If a file I need is missing, inform the user.
 	if ! [ -f "$FILE_BASE" ]; then
 		echo "Missing $FILE_BASE. Did you delete it?" >&2
-		return 0
+		return 1
 	fi
 	if ! [ -f "$FILE_DISK" ]; then
 		echo "Missing $FILE_DISK. Did you delete it?" >&2
-		return 0
+		return 1
 	fi
 	if ! [ -f "$FILE_ENV" ]; then
 		echo "Missing $FILE_ENV. Looks like you forgot to create one." >&2
-		return 0
+		return 1
 	fi
 	# If all files I need exist, then continue
-	return 1
+	return 0
 }
 
 #Checks if already running my containers
@@ -78,22 +78,32 @@ gh_stop() {
 	echo "GAIA HUB stopped."
 }
 
+#Exit on error if the programs I need are not found
+exit_error() {
+   printf "%s" "$1" >&2
+   exit 1
+}
+
+for cmd in grep docker; do
+   command -v "$cmd" >/dev/null 2>&1 || exit_error "Missing command: $cmd"
+done
+
 #Starts GH, Stops GH or displays instructions.
 #Will only execute the start, stop and status if the files I need exist. If they don't it will display a warning.
 case ${TASK} in 
 	start|up)
-		if ! check_files_exist; then
+		if check_files_exist; then
 			echo "will start"
 			gh_start
 		fi
 		;;
 	stop|down)
-		if ! check_files_exist; then
+		if check_files_exist; then
 			gh_stop
 		fi
 		;;
 	status)
-		if ! check_files_exist; then
+		if check_files_exist; then
 			gh_status
 		fi
 		;;
