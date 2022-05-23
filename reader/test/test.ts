9a4eb54b5af86831f8c2a6e6f0790d73bc4f0f43
fritzsima <zh19940321@gmail.com>
2022-05-23 11:20:35 +0800

    fix: filestream error in reader unit tests

--- reader/test/test.ts
+++ reader/test/test.ts
@@ -7,7 +7,7 @@ import { GaiaDiskReader } from '../src/server'
 function testServer() {
   test('check handleGet', (t) => {
     t.plan(8)
-    
+
     const storageDir = `/tmp/gaia-disk-${Math.random()}`
 
     // store /12345/foo/bar.txt
@@ -20,9 +20,9 @@ function testServer() {
     fs.mkdirSync(Path.join(storageDir, '.gaia-metadata'))
     fs.mkdirSync(Path.join(storageDir, '.gaia-metadata/12345'))
     fs.mkdirSync(Path.join(storageDir, '.gaia-metadata/12345/foo'))
-    fs.writeFileSync(Path.join(storageDir, '.gaia-metadata/12345/foo/bar.txt'), 
+    fs.writeFileSync(Path.join(storageDir, '.gaia-metadata/12345/foo/bar.txt'),
       JSON.stringify({'content-type': 'application/potatoes'}))    // bogus mime type for testing
-    
+
     const config = {
       diskSettings: {
         storageRootDirectory: storageDir
@@ -53,7 +53,15 @@ function testServer() {
         t.ok(result.exists, 'file exists')
         t.equal(result.contentType, 'application/octet-stream', 'file has fall-back content type')
 
-        // blow away the file 
+        // close file read stream
+        result.fileReadStream.close()
+        return new Promise((resolve, reject) => {
+          result.fileReadStream.on('close', resolve)
+          result.fileReadStream.on('error', reject)
+        })
+      })
+      .then(() => {
+        // blow away the file
         fs.unlinkSync(Path.join(storageDir, '12345/foo/bar.txt'))
         fs.rmdirSync(Path.join(storageDir, '12345/foo'))
         fs.rmdirSync(Path.join(storageDir, '12345'))
