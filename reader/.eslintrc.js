9983a3c93e0fa617e61948d867e544ea7cc020d5
fritzsima <zh19940321@gmail.com>
2022-06-08 15:38:10 +0800

    refactor: upgrade the reader ts version

--- reader/.eslintrc.js
+++ reader/.eslintrc.js
@@ -66,5 +66,10 @@ module.exports = {
 
     // TODO: enable this when reasonable
     "@typescript-eslint/no-explicit-any": "off",
+
+    "@typescript-eslint/restrict-template-expressions": "off",
+    "@typescript-eslint/no-unsafe-member-access": "off",
+    "@typescript-eslint/no-unsafe-assignment": "off",
+    "@typescript-eslint/no-misused-promises": "off",
   }
 };
