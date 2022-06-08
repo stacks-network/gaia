04af019f699d95dca63018d513cec1ff7ad01d20
fritzsima <zh19940321@gmail.com>
2022-06-08 15:05:52 +0800

    refactor: upgrade the admin ts version

--- admin/.eslintrc.js
+++ admin/.eslintrc.js
@@ -66,5 +66,14 @@ module.exports = {
 
     // TODO: enable this when reasonable
     "@typescript-eslint/no-explicit-any": "off",
+
+    "@typescript-eslint/restrict-template-expressions": "off",
+    "@typescript-eslint/no-unsafe-member-access": "off",
+    "@typescript-eslint/no-unsafe-assignment": "off",
+    "@typescript-eslint/no-unsafe-argument": "off",
+    "@typescript-eslint/no-unsafe-return": "off",
+  },
+  "globals": {
+    "NodeJS": true
   }
 };
