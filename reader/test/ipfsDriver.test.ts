e8033594f9fcbee568264ecb56555aaca61b9ce0
sky <jenakarrasco@gmail.com>
2022-09-16 04:23:34 +0800

    feature: add ipfs driver: reader server ipfsDriver.test.ts

--- reader/test/ipfsDriver.test.ts
+++ reader/test/ipfsDriver.test.ts
@@ -1,6 +1,6 @@
 import * as Path from 'path'
 import { create } from 'ipfs-http-client'
-import { create as createDaemon } from 'ipfs-core'
+import { create as createDaemon, IPFS } from 'ipfs-core'
 import { HttpApi } from 'ipfs-http-server'
 import { HttpGateway } from 'ipfs-http-gateway'
 import { makeECPrivateKey, getPublicKeyFromPrivate, publicKeyToAddress } from '@stacks/encryption'
@@ -11,7 +11,9 @@ import { ReaderServer } from '../src/server.js'
 import IpfsDriver from '../src/drivers/IpfsDriver.js'
 import { getConfig } from '../src/config.js'
 
-test('check ipfs driver handleGet', (done) => {
+
+
+describe('ipfsDriver test', () => {
   const driverConfig = {
     cacheControl: 'no-cache',
     driver: 'ipfs',
@@ -42,81 +44,78 @@ test('check ipfs driver handleGet', (done) => {
     }
   });
 
-  createDaemon()
-    .then(ipfs => {
-      const httpApi = new HttpApi(ipfs)
-      const httpGateway = new HttpGateway(ipfs)
-      return Promise.all([httpApi.start(), httpGateway.start()])
-    })
-    .then(() => {
-      // run ipfs daemon
-      // create the ipfs reader driver
-      const serverConfig = getConfig()
-      let driver = new IpfsDriver(driverConfig)
-      const server = new ReaderServer(driver, serverConfig)
+  var IpfsNode: IPFS
+  var httpApi: HttpApi
+  var httpGateway: HttpGateway
+  var server: ReaderServer
 
+  beforeAll(async () => {
+    // run ipfs daemon
+    IpfsNode = await createDaemon()
+    httpApi = new HttpApi(IpfsNode)
+    httpGateway = new HttpGateway(IpfsNode)
+    await httpApi.start()
+    await httpGateway.start()
+  })
 
+  it('check ipfs driver handleGet', async () => {
+    // create the ipfs reader driver
+    const serverConfig = getConfig()
+    let driver = new IpfsDriver(driverConfig)
+    server = new ReaderServer(driver, serverConfig)
+    
+    /**
+     * START: mock upload file to the ipfs
+     */
+    const IpfsClient = create({ url: driverConfig.ipfsSettings.apiAddress })
+    const privateKey = makeECPrivateKey();
+    const publicKey = getPublicKeyFromPrivate(privateKey);
+    const address = publicKeyToAddress(publicKey);
+    const rndFileName = `${uuidv4()}.txt`
+    const rndFileContent = lorem.generateSentences(5)
+    const METADATA_DIRNAME = '.gaia-metadata'
+    let etag = ''
 
-      /**
-       * START: mock upload file to the ipfs
-       */
-      const IpfsClient = create({ url: driverConfig.ipfsSettings.apiAddress })
-      const privateKey = makeECPrivateKey();
-      const publicKey = getPublicKeyFromPrivate(privateKey);
-      const address = publicKeyToAddress(publicKey);
-      const rndFileName = `${uuidv4()}.txt`
-      const rndFileContent = lorem.generateSentences(5)
-      const METADATA_DIRNAME = '.gaia-metadata'
-      let etag = ''
+    const absoluteFilePath = Path.join(driverConfig.ipfsSettings.storageRootDirectory, address, rndFileName)
+    const metaDataFilePath = Path.join(driverConfig.ipfsSettings.storageRootDirectory, METADATA_DIRNAME, address, rndFileName)
 
-      const absoluteFilePath = Path.join(driverConfig.ipfsSettings.storageRootDirectory, address, rndFileName)
-      const metaDataFilePath = Path.join(driverConfig.ipfsSettings.storageRootDirectory, METADATA_DIRNAME, address, rndFileName)
+    const absdirname = Path.dirname(absoluteFilePath)
+    const normalizedPath = Path.normalize(absdirname)
+    // Ensures that the directory exists. If the directory structure does not exist, it is created. Like mkdir -p.
+    await IpfsClient.files.mkdir(normalizedPath, { parents: true })
+    await IpfsClient.files.write(absoluteFilePath, rndFileContent, { create: true, mode: 0o600 })
+    let stat = await IpfsClient.files.stat(absoluteFilePath)
 
-      const absdirname = Path.dirname(absoluteFilePath)
-      const normalizedPath = Path.normalize(absdirname)
-      // Ensures that the directory exists. If the directory structure does not exist, it is created. Like mkdir -p.
-      IpfsClient.files.mkdir(normalizedPath, { parents: true })
-        .then(() => {
-          return IpfsClient.files.write(absoluteFilePath, rndFileContent, { create: true, mode: 0o600 })
-        })
-        .then(() => {
-          return IpfsClient.files.stat(absoluteFilePath)
-        })
-        .then((stat) => {
-          etag = stat.cid.toV1().toString(base16)
-          const metaDataDirPath = Path.dirname(metaDataFilePath)
-          return IpfsClient.files.mkdir(metaDataDirPath)
-        })
-        .then(() => {
-          return IpfsClient.files.write(metaDataFilePath, JSON.stringify({ etag }), { create: true, mode: 0o600 })
-        })
-        .then(() => {
-          /**
-          * END: mock upload file to the ipfs
-          */
-          server.handleGet(address, rndFileName)
-            .then(async (result) => {
-              // file exists
-              expect(result.exists).toBeTruthy()
-              // file etag to be equal
-              expect(result.etag).toEqual(etag)
-              let fileContent
-              if(result.fileReadStream) {
-                for await (const chunk of result.fileReadStream) {
-                  fileContent = chunk.toString()
-                }
-                // file content to be equal ranFileContent
-                expect(fileContent).toEqual(rndFileContent)
-              }
-              return server.handleGet(publicKeyToAddress(getPublicKeyFromPrivate(makeECPrivateKey())), rndFileName)
-            })
-            .then((result) => {
-              
-              // file shouldn't exists
-              expect(result.exists).toBeFalsy()
-              done()
-            })
-        })
-    })
+    etag = stat.cid.toV1().toString(base16)
+    const metaDataDirPath = Path.dirname(metaDataFilePath)
+    
+    await IpfsClient.files.mkdir(metaDataDirPath)
+    await IpfsClient.files.write(metaDataFilePath, JSON.stringify({ etag }), { create: true, mode: 0o600 })
+    /**
+    * END: mock upload file to the ipfs
+    */
+    let result1 = await server.handleGet(address, rndFileName)
+    // file exists
+    expect(result1.exists).toBeTruthy()
+    // file etag to be equal
+    expect(result1.etag).toEqual(etag)
+    let fileContent
+    if (result1.fileReadStream) {
+      for await (const chunk of result1.fileReadStream) {
+        fileContent = chunk.toString()
+      }
+      // file content to be equal ranFileContent
+      expect(fileContent).toEqual(rndFileContent)
+    }
+    let result2 = await server.handleGet(publicKeyToAddress(getPublicKeyFromPrivate(makeECPrivateKey())), rndFileName)
+    // file shouldn't exists
+    expect(result2.exists).toBeFalsy()
+  })
+
+  afterAll((done) => {
+    httpApi.stop()
+      .then(() => httpGateway.stop())
+      .then(() => IpfsNode.stop())
+      .then(() => done())
+  })
 })
-
