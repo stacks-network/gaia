c1216c424c344626d7d6f18a4827749b71d31b61
fritzsima <zh19940321@gmail.com>
2022-10-14 16:26:21 +0800

    fix: jest, lint config

--- dashboard/backend/jest.config.ts
+++ dashboard/backend/jest.config.ts
@@ -88,7 +88,9 @@ export default {
   // ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-  // moduleNameMapper: {},
+  moduleNameMapper: {
+    "(.*)\\.js$": "$1"
+  },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
@@ -100,7 +102,7 @@ export default {
   // notifyMode: "failure-change",
 
   // A preset that is used as a base for Jest's configuration
-  // preset: undefined,
+  preset: 'ts-jest',
 
   // Run tests from one or more projects
   // projects: undefined,
